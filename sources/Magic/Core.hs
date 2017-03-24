3f67b6890528d9d8ce8b7480fb5d7e8a54702ce5
Spiros Boosalis <SamBoosalis@gmail.com>
2017-03-24 04:56:43 +0000

    rect

--- sources/Magic/Core.hs
+++ sources/Magic/Core.hs
@@ -1,7 +1,17 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
 module Magic.Core where
 import Magic.Extra
 import Magic.Types
 
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
+
 import Prelude.Spiros
+
+-- 63 x 88 mm
+cardDiagram :: Diagram B
+cardDiagram = rect 5 7 & fc blue
 
--- sources/Magic/Example.hs
+++ sources/Magic/Example.hs
@@ -1,21 +1,29 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-} -- to test inference
 module Magic.Example where
-import Magic()
+import Magic
 import System.Environment
+
+import Diagrams.Prelude
+import Diagrams.Backend.SVG.CmdLine
 
 {-|
 @
-stack build && stack exec -- example-magic-card-editor
+
+stack build && stack exec -- example-magic-card-editor -o circle.svg -w 400 && open circle.svg
+
+stack build && stack exec -- example-magic-card-editor -o circle.svg -w 250
+
 @
 -}
 main :: IO ()
 main = do
- arguments <- getArgs >>= \case
-  [s] -> return (s)
-  _ -> return ("")
- mainWith arguments
+--  arguments <- getArgs >>= \case
+--   [s] -> return (s)
+--   _ -> return ("")
+--  main' arguments
 
-main s = do
- putStrLn s
+-- main' s = do
+--  putStrLn s
  putStrLn "(Magic.Example...)"
+ mainWith cardDiagram
 
